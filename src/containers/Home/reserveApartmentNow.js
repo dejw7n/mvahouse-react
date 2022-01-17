789873b601d8f05697de14915a016d2de5569bef
Dejw <dejw7n@gmail.com>
2022-01-17 23:18:24 +0100

    Update Home.js

--- src/containers/Home/reserveApartmentNow.js
+++ src/containers/Home/reserveApartmentNow.js
@@ -68,9 +68,9 @@ const product = {
 		{ name: "2XL", inStock: true },
 		{ name: "3XL", inStock: true },
 	],
-	description: 'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-	highlights: ["Hand cut and sewn locally", "Dyed with our proprietary colors", "Pre-washed & pre-shrunk", "Ultra-soft 100% cotton"],
-	details: 'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+	description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Donec vitae arcu. Integer imperdiet lectus quis justo.",
+	highlights: ["", "", "", ""],
+	details: "",
 };
 const reviews = { href: "#", average: 4, totalCount: 17 };
 
