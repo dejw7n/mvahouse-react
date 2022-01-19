d64e437190c78a40cb1e179f0f9ce1f7d933c7ff
Dejw <dejw7n@gmail.com>
2022-01-19 21:38:48 +0100

    Update Home.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,10 @@ import logoIcon from "../../assets/img/logoIcon.png";
 import logoText from "../../assets/img/logoText.png";
 
 const navigation = [
-	{ name: "Dashboard", href: "#", current: true },
-	{ name: "Team", href: "#", current: false },
-	{ name: "Projects", href: "#", current: false },
-	{ name: "Calendar", href: "#", current: false },
+	{ name: "Home", href: "#", current: true },
+	{ name: "About us", href: "#", current: false },
+	{ name: "Gallery", href: "#", current: false },
+	{ name: "Contact", href: "#", current: false },
 ];
 
 function classNames(...classes) {
@@ -55,7 +55,7 @@ export default function Example() {
 									<div>
 										<Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
 											<span className="sr-only">Open user menu</span>
-											<img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+											<img className="h-8 w-8 rounded-full" src="" alt="" />
 										</Menu.Button>
 									</div>
 									<Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -11,22 +11,22 @@ function Home() {
 		{
 			name: "Botanická zahrada",
 			description: "400m",
-			imageSrc: "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg",
-			imageAlt: "Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.",
+			imageSrc: "https://www.kudyznudy.cz/files/d1/d1bb6357-688e-4720-8c5e-b0270a686cf1.jpg?v=20210714072230",
+			imageAlt: "",
 			href: "#",
 		},
 		{
 			name: "St. Clari",
 			description: "950m",
-			imageSrc: "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg",
-			imageAlt: "Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.",
+			imageSrc: "https://www.kudyznudy.cz/files/d1/d1bb6357-688e-4720-8c5e-b0270a686cf1.jpg?v=20210714072230",
+			imageAlt: "",
 			href: "#",
 		},
 		{
 			name: "Doubravská Hora",
 			description: "1,7km",
-			imageSrc: "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg",
-			imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
+			imageSrc: "https://www.kudyznudy.cz/files/d1/d1bb6357-688e-4720-8c5e-b0270a686cf1.jpg?v=20210714072230",
+			imageAlt: "",
 			href: "#",
 		},
 	];
