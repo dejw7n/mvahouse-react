a1847806658296d2fcf095d319927bb07bcc2906
Dejw <dejw7n@gmail.com>
2022-01-27 15:29:09 +0100

    Update API

--- src/components/ApartmentManager/ApartmentManager.js
+++ src/components/ApartmentManager/ApartmentManager.js
@@ -1,22 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 export function GetApartment(link) {
-    const [apartment, setApartment] = useState([]);
-    useEffect(() => {
-        Axios.get(`http://192.168.10.2:3001/api/getApartment/${link}`).then((response) => {
-            setApartment(response.data);
-        });
-    }, []);
-    return apartment;
+	const [apartment, setApartment] = useState([]);
+	useEffect(() => {
+		Axios.get(`http://194.87.139.53:30000/api/getApartment/${link}`).then((response) => {
+			setApartment(response.data);
+		});
+	}, []);
+	return apartment;
 }
 
 export function GetAllApartments() {
-    const [apartmentList, setApartmentList] = useState([]);
-    useEffect(() => {
-        Axios.get(`http://192.168.10.2:3001/api/getAllApartments/`).then((response) => {
-            setApartmentList(response.data);
-        });
-    }, []);
-    return apartmentList;
+	const [apartmentList, setApartmentList] = useState([]);
+	useEffect(() => {
+		Axios.get(`http://194.87.139.53:30000/api/getAllApartments/`).then((response) => {
+			setApartmentList(response.data);
+		});
+	}, []);
+	return apartmentList;
 }
