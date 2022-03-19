c0e3cb57f894f449a1c2bc8047b20a46f92ca4a5
David <dejw7n@gmail.com>
2022-03-19 11:25:02 +0100

    Add load listener after page load

--- public/assets/js/gallery.js
+++ public/assets/js/gallery.js
@@ -1,38 +1,44 @@
-var items = document.getElementById("gallery").querySelectorAll("#gallery-item");
-var itemsImg = document.getElementById("gallery").querySelectorAll("#gallery-img");
-var galleryDisplay = document.getElementById("gallery-display");
-var galleryDisplayImg = document.getElementById("gallery-display-img");
-var galleryDisplay__left__closeBtn = document.getElementById("gallery-display__left__closeBtn");
-var galleryDisplay__left__previousBtn = document.getElementById("gallery-display__left__previousBtn");
-var galleryDisplay__right__nextBtn = document.getElementById("gallery-display__right__nextBtn");
+window.addEventListener(
+	"load",
+	function () {
+		var items = document.getElementById("gallery").querySelectorAll("#gallery-item");
+		var itemsImg = document.getElementById("gallery").querySelectorAll("#gallery-img");
+		var galleryDisplay = document.getElementById("gallery-display");
+		var galleryDisplayImg = document.getElementById("gallery-display-img");
+		var galleryDisplay__left__closeBtn = document.getElementById("gallery-display__left__closeBtn");
+		var galleryDisplay__left__previousBtn = document.getElementById("gallery-display__left__previousBtn");
+		var galleryDisplay__right__nextBtn = document.getElementById("gallery-display__right__nextBtn");
 
-for (let x = 0; x < items.length; x++) {
-	items[x].addEventListener("click", function () {
-		//hideHeader();
-		galleryDisplayImg.src = itemsImg[x].src;
-		galleryDisplay.setAttribute("gallery-img", x);
-		document.body.style.overflow = "hidden";
-		galleryDisplay.style.display = "block";
-	});
-}
-function nextImg(x) {
-	let nextImg = parseInt(galleryDisplay.getAttribute("gallery-img")) + x;
-	if (nextImg < 0) {
-		nextImg = items.length - 1;
-	}
-	if (nextImg > items.length - 1) {
-		nextImg = 0;
-	}
-	galleryDisplayImg.src = itemsImg[nextImg].src;
-	galleryDisplay.setAttribute("gallery-img", nextImg);
-}
-galleryDisplay__left__closeBtn.addEventListener("click", function () {
-	galleryDisplay.style.display = "none";
-	document.body.style.overflow = "unset";
-});
-galleryDisplay__left__previousBtn.addEventListener("click", function () {
-	nextImg(-1);
-});
-galleryDisplay__right__nextBtn.addEventListener("click", function () {
-	nextImg(1);
-});
+		for (let x = 0; x < items.length; x++) {
+			items[x].addEventListener("click", function () {
+				//hideHeader();
+				galleryDisplayImg.src = itemsImg[x].src;
+				galleryDisplay.setAttribute("gallery-img", x);
+				document.body.style.overflow = "hidden";
+				galleryDisplay.style.display = "block";
+			});
+		}
+		function nextImg(x) {
+			let nextImg = parseInt(galleryDisplay.getAttribute("gallery-img")) + x;
+			if (nextImg < 0) {
+				nextImg = items.length - 1;
+			}
+			if (nextImg > items.length - 1) {
+				nextImg = 0;
+			}
+			galleryDisplayImg.src = itemsImg[nextImg].src;
+			galleryDisplay.setAttribute("gallery-img", nextImg);
+		}
+		galleryDisplay__left__closeBtn.addEventListener("click", function () {
+			galleryDisplay.style.display = "none";
+			document.body.style.overflow = "unset";
+		});
+		galleryDisplay__left__previousBtn.addEventListener("click", function () {
+			nextImg(-1);
+		});
+		galleryDisplay__right__nextBtn.addEventListener("click", function () {
+			nextImg(1);
+		});
+	},
+	false
+);
